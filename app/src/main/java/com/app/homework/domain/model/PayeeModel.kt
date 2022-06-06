8fac9512eb614c386ed0ad66c028125022387739
user-mac-glit-001 <kims.batik@gmail.com>
2022-06-06 10:30:04 +0700

    Add the data class payee id, name and accountNo

--- app/src/main/java/com/app/homework/domain/model/PayeeModel.kt
+++ app/src/main/java/com/app/homework/domain/model/PayeeModel.kt
@@ -2,7 +2,7 @@ package com.app.homework.domain.model
 
 data class PayeeResponseModel(val status : String,val data : List<Payee>)
 
-data class Payee(val id : String,
-                 val name : String,
-                 val accountNo : String)
+data class Payee(val accountNo: String,
+                 val id: String,
+                 val name: String)
 
