c1902c6a451151293674b28784ac2ecca30cba21
user-mac-glit-001 <kims.batik@gmail.com>
2022-06-06 10:29:42 +0700

    Add the constructor of main

--- app/src/main/java/com/app/homework/domain/MainRepository.kt
+++ app/src/main/java/com/app/homework/domain/MainRepository.kt
@@ -11,8 +11,8 @@ class MainRepository  constructor(private val apiService: ApiService) {
 
     suspend fun doLogin(loginRequest : LoginRequest) = apiService.doLogin(loginRequest)
     suspend fun doSignUp(signUpRequest : SignUpRequest) = apiService.doSignUp(signUpRequest)
+    suspend fun doTransfer(token : String,transferRequestModel : TransferRequestModel) = apiService.doTransfer(token,transferRequestModel)
     suspend fun getTransactions(token : String) = apiService.getTransactions(token)
     suspend fun getAccountBalance(token : String) = apiService.getAccountBalance(token)
     suspend fun getPayeeList(token : String) = apiService.getPayeeList(token)
-    suspend fun doTransfer(token : String,transferRequestModel : TransferRequestModel) = apiService.doTransfer(token,transferRequestModel)
-}
+ }
