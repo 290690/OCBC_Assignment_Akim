431563515a6309726aeacf291c6c64490a49e868
user-mac-glit-001 <kims.batik@gmail.com>
2022-06-06 10:31:41 +0700

    add data class of Transactionlist and recipient

--- app/src/main/java/com/app/homework/domain/model/TransactionModel.kt
+++ app/src/main/java/com/app/homework/domain/model/TransactionModel.kt
@@ -3,8 +3,8 @@ package com.app.homework.domain.model
 data class TransactionResponse(val status : String,
                                val data : List<TransactionList>)
 
-data class TransactionList(val amount : Double,
-                           val transactionDate : String,
+data class TransactionList(val transactionDate : String,
+                           val amount : Double,
                            val receipient : TransactionRecipient)
 
 data class TransactionRecipient(val accountNo : String = "",
