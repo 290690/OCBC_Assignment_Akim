8f6b052ecac4ea1684d676e1ace36ac8107ef859
user-mac-glit-001 <kims.batik@gmail.com>
2022-06-06 10:20:40 +0700

    add data class of BalanceResponseModel

--- app/src/main/java/com/app/homework/domain/model/BalanceModel.kt
+++ app/src/main/java/com/app/homework/domain/model/BalanceModel.kt
@@ -2,6 +2,6 @@ package com.app.homework.domain.model
 
 import com.app.homework.domain.Response
 
-data class BalanceResponseModel(val status : String,
-                                val accountNo : String,
-                                val balance : String)
+data class BalanceResponseModel(val balance: String,
+                                val status: String,
+                                val accountNo: String)
