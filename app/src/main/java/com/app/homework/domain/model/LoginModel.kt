e8ee6ddadf6433d0bc1692398b6a1668a5c3cf86
user-mac-glit-001 <kims.batik@gmail.com>
2022-06-06 10:29:18 +0700

    add data class Login Response and Value

--- app/src/main/java/com/app/homework/domain/model/LoginModel.kt
+++ app/src/main/java/com/app/homework/domain/model/LoginModel.kt
@@ -3,7 +3,7 @@ package com.app.homework.domain.model
 data class LoginRequest(val username : String,
                         val password : String)
 
-data class LoginResponse(val status : String,
-                         val token : String,
-                         val username : String,
-                         val accountNo : String)
+data class LoginResponse(val token: String,
+                         val accountNo: String,
+                         val username: String,
+                         val status: String)
